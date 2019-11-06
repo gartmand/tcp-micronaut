05dbb8c6a2597fef8b1869ad6773896a9d0b6a56
Dale Gartman <dale.gartman@excella.com>
2019-11-06 11:50:55 -0500

    Swap Employees to use Mongo. Add basic docker-compose file.

--- employees/src/main/kotlin/dev/gartman/EmployeeController.kt
+++ employees/src/main/kotlin/dev/gartman/EmployeeController.kt
@@ -1,14 +1,13 @@
 package dev.gartman
 
+import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.HttpStatus
 
 @Controller("/employee")
 class EmployeeController {
 
     @Get("/")
-
     fun index(): HttpStatus {
         return HttpStatus.OK
     }
